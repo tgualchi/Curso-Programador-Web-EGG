90aa7dfdc121c4a65188531282132500d7be1ab5
tgualchi <tobgual@gmail.com>
2023-11-15 13:32:50 -0300

    cambio de formulario
    el formulario se abrira en un nuevo link

--- miprimerapagina/script.js
+++ miprimerapagina/script.js
@@ -23,34 +23,3 @@ function cambiarTexto1 (nombreApellido) {
 
 parrafo2.innerText = cambiarTexto (estudio, lugarDeEstudio);
 parrafo1.innerText = cambiarTexto1 (nombreApellido);
-
-const form = document.getElementById("form");
-const nombre = document.getElementById("nombre");
-const parrafo = document.getElementById("alertas");
-
-function validarFormulario() {
-  let warnings = "";
-  let valido = true;
-  parrafo.innerHTML = "";
-
-  if (nombre.value.length < 4) {
-    warnings += `El nombre debe contener más de 4 carcateres`;
-    valido = false;
-  }
-
-  if (!valido) {
-    parrafo.innerHTML = warnings;
-  } else {
-    parrafo.innerHTML = "Enviado";
-  }
-  return valido;
-}
-
-form.addEventListener("submit", (e) => {
-  if (validarFormulario()) {
-    // Si la validación es exitosa, puedes enviar el formulario
-    formulario.submit();
-  } else {
-    e.preventDefault(); // Evita que el formulario se envíe automáticamente
-  }
-});
